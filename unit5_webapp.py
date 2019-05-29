acc02acdb0b492c7a93829e9c68e592c33f4f8cd
Bartlomiej Krol <bartlomiej.krol@consonance.tech>
2019-05-29 20:01:30 +0200

    ready form and bug with sqllite

--- unit5_webapp.py
+++ unit5_webapp.py
@@ -16,18 +16,20 @@ class Formdata(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now)
     firstname = db.Column(db.String, nullable=False)
     email = db.Column(db.String)
-    age = db.Column(db.Integer)
-    income = db.Column(db.Integer)
-    satisfaction = db.Column(db.Integer)
+    iff = db.Column(db.String)
+    which = db.Column(db.String)
+    if_now = db.Column(db.String)
+    type = db.Column(db.String)
     q1 = db.Column(db.Integer)
     q2 = db.Column(db.Integer)
 
-    def __init__(self, firstname, email, age, income, satisfaction, q1, q2):
+    def __init__(self, firstname, email, iff, which, if_now, type, q1, q2):
         self.firstname = firstname
         self.email = email
-        self.age = age
-        self.income = income
-        self.satisfaction = satisfaction
+        self.iff = iff
+        self.which = which
+        self.if_now = if_now
+        self.type = type
         self.q1 = q1
         self.q2 = q2
 
@@ -53,31 +55,38 @@ def show_result():
     fd_list = db.session.query(Formdata).all()
 
     # Some simple statistics for sample questions
-    satisfaction = []
+    iff = []
+    which = []
+    if_now = []
+    type = []
     q1 = []
     q2 = []
     for el in fd_list:
-        satisfaction.append(int(el.satisfaction))
-        q1.append(int(el.q1))
-        q2.append(int(el.q2))
+        iff.append(int(el.iff))
+        which.append(el.which)
+        if_now.append(el.if_now)
+        type.append(el.type)
+        q1.append((el.q1))
+        q2.append((el.q2))
 
-    if len(satisfaction) > 0:
-        mean_satisfaction = statistics.mean(satisfaction)
+    if len(iff) > 0:
+        # mean_satisfaction = statistics.mean(satisfaction)
+        mean_iff = iff.count('Tak')
     else:
-        mean_satisfaction = 0
+        mean_iff = 0
 
-    if len(q1) > 0:
-        mean_q1 = statistics.mean(q1)
-    else:
-        mean_q1 = 0
-
-    if len(q2) > 0:
-        mean_q2 = statistics.mean(q2)
-    else:
-        mean_q2 = 0
+    # if len(q1) > 0:
+    #     mean_q1 = statistics.mean(q1)
+    # else:
+    #     mean_q1 = 0
+    #
+    # if len(q2) > 0:
+    #     mean_q2 = statistics.mean(q2)
+    # else:
+    #     mean_q2 = 0
 
     # Prepare data for google charts
-    data = [['Satisfaction', mean_satisfaction], ['Python skill', mean_q1], ['Flask skill', mean_q2]]
+    data = [['Czy grałeś', mean_iff]]
 
     return render_template('result.html', data=data)
 
@@ -87,14 +96,15 @@ def save():
     # Get data from FORM
     firstname = request.form['firstname']
     email = request.form['email']
-    age = request.form['age']
-    income = request.form['income']
-    satisfaction = request.form['satisfaction']
+    iff = request.form['if']
+    which = request.form['which']
+    if_now = request.form['if_now']
+    type = request.form['type']
     q1 = request.form['q1']
     q2 = request.form['q2']
 
     # Save the data
-    fd = Formdata(firstname, email, age, income, satisfaction, q1, q2)
+    fd = Formdata(firstname, email, iff, which, if_now, type, q1, q2)
     db.session.add(fd)
     db.session.commit()
 
@@ -102,4 +112,4 @@ def save():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    app.run(debug=True, use_reloader=False)
